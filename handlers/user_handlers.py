dd26668b889f8b933b5826c2e6d2082ec1e0fef5
dastankg <astan12324@gmail.com>
2024-07-08 18:17:56 +0600

    code optimization

--- handlers/user_handlers.py
+++ handlers/user_handlers.py
@@ -1,3 +1,6 @@
+import asyncio
+
+import aiohttp
 from aiogram import F, Router
 from aiogram.filters import Command, CommandStart, StateFilter
 from aiogram.types import Message
@@ -58,7 +61,6 @@ async def process_cancel(message: Message, state: FSMContext):
 async def fill_name(message: Message, state: FSMContext):
     await state.update_data(name=message.text)
     nick = await state.get_data()
-    print(nick)
     url = f"https://leetcode-api-faisalshohag.vercel.app/{nick['name']}"
     response = requests.get(url)
     if 'errors' in response.json():
@@ -75,31 +77,40 @@ async def fill_name(message: Message, state: FSMContext):
 @router.message(Command(commands='rankByContest'), StateFilter(default_state))
 async def rankByContest(message: Message):
     cur = conn.cursor()
-    cur.execute(f"SELECT * FROM users WHERE user_id={message.from_user.id}")
+    cur.execute(f"SELECT login FROM users WHERE user_id={message.from_user.id}")
     rows = cur.fetchall()
-    id = rows[0][0]
-    user_id = rows[0][1]
-    login = rows[0][2]
+    id = message.from_user.id
+    login = rows[0][0]
     lst = [[0, login]]
     cur.execute(f"SELECT * FROM friends WHERE id={id}")
     rows = cur.fetchall()
     for i in range(len(rows)):
         lst.append([0, rows[i][1], 0, 0, 0])
-    for i in range(len(lst)):
-        url = 'https://leetcode.com/graphql?query=query { userContestRanking(username:' + '"' + lst[i][
-            1] + '"' + ') { attendedContestsCount rating globalRanking totalParticipants topPercentage } }'
 
-        response = requests.get(url)
+    async def fetch_total_solved(session, username):
+        url = "https://leetcode.com/graphql"
+        query = """
+                   {
+                       userContestRanking(username:  "%s")  {
+                            rating
+                        }
+                   }
+                   """ % username
+        async with session.post(url, json={'query': query}) as response:
+            data = await response.json()
+            if data['data']['userContestRanking'] is None:
+                return 0
+            return data['data']['userContestRanking']['rating']
 
-        data = response.json()
-
-        if data['data']['userContestRanking'] is not None:
-            lst[i][0] = data['data']['userContestRanking']['rating']
+    async with aiohttp.ClientSession() as session:
+        tasks = [fetch_total_solved(session, user[1]) for user in lst]
+        results = await asyncio.gather(*tasks)
+        for i, totalSolved in enumerate(results):
+            lst[i][0] = totalSolved
 
     lst.sort(reverse=True)
     ans = '```\n'
-
-    ans += f"{'№':<3} | {'Имя':<15} | {'Ranking':<15}\n"
+    ans += f"{'№':<3} | {'Имя':<15} | {'totalSolved':<15}\n"
     ans += '-' * 30 + '\n'
 
     for j in range(len(lst)):
@@ -115,18 +126,17 @@ async def rankByContest(message: Message):
 @router.message(Command(commands='rankByTotalProblem'), StateFilter(default_state))
 async def rankByContest(message: Message):
     cur = conn.cursor()
-    cur.execute(f"SELECT * FROM users WHERE user_id={message.from_user.id}")
+    cur.execute(f"SELECT login FROM users WHERE user_id={message.from_user.id}")
     rows = cur.fetchall()
-    id = rows[0][0]
-    user_id = rows[0][1]
-    login = rows[0][2]
+    id = message.from_user.id
+    login = rows[0][0]
     lst = [[0, login]]
     cur.execute(f"SELECT * FROM friends WHERE id={id}")
     rows = cur.fetchall()
     for i in range(len(rows)):
         lst.append([0, rows[i][1]])
-    for i in range(len(lst)):
-        username = lst[i][1]
+
+    async def fetch_total_solved(session, username):
         url = "https://leetcode.com/graphql"
         query = """
            {
@@ -140,11 +150,15 @@ async def rankByContest(message: Message):
              }
            }
            """ % username
-        response = requests.post(url, json={'query': query})
+        async with session.post(url, json={'query': query}) as response:
+            data = await response.json()
+            return data['data']['matchedUser']['submitStats']['acSubmissionNum'][0]['count']
 
-        data = response.json()
-        totalSolved = data['data']['matchedUser']['submitStats']['acSubmissionNum'][0]['count']
-        lst[i][0] = totalSolved
+    async with aiohttp.ClientSession() as session:
+        task = [fetch_total_solved(session, user[1]) for user in lst]
+        result = await asyncio.gather(*task)
+        for i, totalSolved in enumerate(result):
+            lst[i][0] = totalSolved
 
     lst.sort(reverse=True)
     ans = '```\n'
@@ -164,36 +178,39 @@ async def rankByContest(message: Message):
 @router.message(Command(commands='rankByEasyProblem'), StateFilter(default_state))
 async def rankByContest(message: Message):
     cur = conn.cursor()
-    cur.execute(f"SELECT * FROM users WHERE user_id={message.from_user.id}")
+    cur.execute(f"SELECT login FROM users WHERE user_id={message.from_user.id}")
     rows = cur.fetchall()
-    id = rows[0][0]
-    user_id = rows[0][1]
-    login = rows[0][2]
+    id = message.from_user.id
+    login = rows[0][0]
     lst = [[0, login]]
     cur.execute(f"SELECT * FROM friends WHERE id={id}")
     rows = cur.fetchall()
     for i in range(len(rows)):
         lst.append([0, rows[i][1]])
-    for i in range(len(lst)):
-        username = lst[i][1]
+
+    async def fetch_total_solved(session, username):
         url = "https://leetcode.com/graphql"
         query = """
-                   {
-                     matchedUser(username: "%s") {
-                       username
-                       submitStats: submitStatsGlobal {
-                         acSubmissionNum {
-                           count
-                         }
-                       }
-                     }
-                   }
-                   """ % username
-        response = requests.post(url, json={'query': query})
+              {
+                matchedUser(username: "%s") {
+                  username
+                  submitStats: submitStatsGlobal {
+                    acSubmissionNum {
+                      count
+                    }
+                  }
+                }
+              }
+              """ % username
+        async with session.post(url, json={'query': query}) as response:
+            data = await response.json()
+            return data['data']['matchedUser']['submitStats']['acSubmissionNum'][1]['count']
 
-        data = response.json()
-        totalSolved = data['data']['matchedUser']['submitStats']['acSubmissionNum'][1]['count']
-        lst[i][0] = totalSolved
+    async with aiohttp.ClientSession() as session:
+        task = [fetch_total_solved(session, user[1]) for user in lst]
+        result = await asyncio.gather(*task)
+        for i, totalSolved in enumerate(result):
+            lst[i][0] = totalSolved
 
     lst.sort(reverse=True)
     ans = '```\n'
@@ -213,36 +230,39 @@ async def rankByContest(message: Message):
 @router.message(Command(commands='rankByMediumProblem'), StateFilter(default_state))
 async def rankByContest(message: Message):
     cur = conn.cursor()
-    cur.execute(f"SELECT * FROM users WHERE user_id={message.from_user.id}")
+    cur.execute(f"SELECT login FROM users WHERE user_id={message.from_user.id}")
     rows = cur.fetchall()
-    id = rows[0][0]
-    user_id = rows[0][1]
-    login = rows[0][2]
+    id = message.from_user.id
+    login = rows[0][0]
     lst = [[0, login]]
     cur.execute(f"SELECT * FROM friends WHERE id={id}")
     rows = cur.fetchall()
     for i in range(len(rows)):
         lst.append([0, rows[i][1]])
-    for i in range(len(lst)):
-        username = lst[i][1]
+
+    async def fetch_total_solved(session, username):
         url = "https://leetcode.com/graphql"
         query = """
-                           {
-                             matchedUser(username: "%s") {
-                               username
-                               submitStats: submitStatsGlobal {
-                                 acSubmissionNum {
-                                   count
-                                 }
-                               }
-                             }
-                           }
-                           """ % username
-        response = requests.post(url, json={'query': query})
+              {
+                matchedUser(username: "%s") {
+                  username
+                  submitStats: submitStatsGlobal {
+                    acSubmissionNum {
+                      count
+                    }
+                  }
+                }
+              }
+              """ % username
+        async with session.post(url, json={'query': query}) as response:
+            data = await response.json()
+            return data['data']['matchedUser']['submitStats']['acSubmissionNum'][2]['count']
 
-        data = response.json()
-        totalSolved = data['data']['matchedUser']['submitStats']['acSubmissionNum'][2]['count']
-        lst[i][0] = totalSolved
+    async with aiohttp.ClientSession() as session:
+        task = [fetch_total_solved(session, user[1]) for user in lst]
+        result = await asyncio.gather(*task)
+        for i, totalSolved in enumerate(result):
+            lst[i][0] = totalSolved
 
     lst.sort(reverse=True)
     ans = '```\n'
@@ -262,36 +282,39 @@ async def rankByContest(message: Message):
 @router.message(Command(commands='rankByHardProblem'), StateFilter(default_state))
 async def rankByContest(message: Message):
     cur = conn.cursor()
-    cur.execute(f"SELECT * FROM users WHERE user_id={message.from_user.id}")
+    cur.execute(f"SELECT login FROM users WHERE user_id={message.from_user.id}")
     rows = cur.fetchall()
-    id = rows[0][0]
-    user_id = rows[0][1]
-    login = rows[0][2]
+    id = message.from_user.id
+    login = rows[0][0]
     lst = [[0, login]]
     cur.execute(f"SELECT * FROM friends WHERE id={id}")
     rows = cur.fetchall()
     for i in range(len(rows)):
         lst.append([0, rows[i][1]])
-    for i in range(len(lst)):
-        username = lst[i][1]
+
+    async def fetch_total_solved(session, username):
         url = "https://leetcode.com/graphql"
         query = """
-                                   {
-                                     matchedUser(username: "%s") {
-                                       username
-                                       submitStats: submitStatsGlobal {
-                                         acSubmissionNum {
-                                           count
-                                         }
-                                       }
-                                     }
-                                   }
-                                   """ % username
-        response = requests.post(url, json={'query': query})
+              {
+                matchedUser(username: "%s") {
+                  username
+                  submitStats: submitStatsGlobal {
+                    acSubmissionNum {
+                      count
+                    }
+                  }
+                }
+              }
+              """ % username
+        async with session.post(url, json={'query': query}) as response:
+            data = await response.json()
+            return data['data']['matchedUser']['submitStats']['acSubmissionNum'][3]['count']
 
-        data = response.json()
-        totalSolved = data['data']['matchedUser']['submitStats']['acSubmissionNum'][3]['count']
-        lst[i][0] = totalSolved
+    async with aiohttp.ClientSession() as session:
+        task = [fetch_total_solved(session, user[1]) for user in lst]
+        result = await asyncio.gather(*task)
+        for i, totalSolved in enumerate(result):
+            lst[i][0] = totalSolved
 
     lst.sort(reverse=True)
     ans = '```\n'
@@ -316,26 +339,27 @@ async def addFriends(message: Message, state: FSMContext):
 
 @router.message(StateFilter(FSMFillForm.fill_friends))
 async def fill_friends(message: Message, state: FSMContext):
-    cur = conn.cursor()
-    cur.execute(f"SELECT * FROM users WHERE user_id={message.from_user.id}")
-    rows = cur.fetchall()
-    id = rows[0][0]
-    user_id = rows[0][1]
-    login = rows[0][2]
-    lst = [[0, login]]
-
+    id = message.from_user.id
     await state.update_data(name=message.text)
     nick = await state.get_data()
     print(nick)
-    url = f"https://leetcode-api-faisalshohag.vercel.app/{nick['name']}"
-    response = requests.get(url)
+    url = "https://leetcode.com/graphql"
+    query = """
+               {
+                   userContestRanking(username:  "%s")  {
+                        rating
+                    }
+               }
+               """ % nick['name']
+    response = requests.post(url, json={'query': query})
+    print(response.json())
     if 'errors' in response.json():
         await message.answer(text=LEXICON['invalid_username'])
     else:
+        cur = conn.cursor()
         cur.execute("INSERT INTO friends (user_name, id) VALUES (%s, %s)", (nick['name'], id))
         conn.commit()
         await message.answer(text=LEXICON['friend_added'])
-        await message.answer(text=LEXICON['/help'])
         await state.clear()
 
 
@@ -348,9 +372,7 @@ async def deleteFriends(message: Message, state: FSMContext):
 @router.message(StateFilter(FSMFillForm.delete_friends))
 async def delete_friends(message: Message, state: FSMContext):
     cur = conn.cursor()
-    cur.execute(f"SELECT id FROM users WHERE user_id={message.from_user.id}")
-    rows = cur.fetchall()
-    id = rows[0][0]
+    id = message.from_user.id
     await state.update_data(name=message.text)
     nick = await state.get_data()
     query = "DELETE FROM friends WHERE id = %s AND user_name = %s"
--- test.py
+++ test.py
@@ -1,23 +1,37 @@
 import requests
-username = "SatoruGodjo"
+
+username = "pratovv"
+# url = "https://leetcode.com/graphql"
+# query = """
+#    {
+#      matchedUser(username: "%s") {
+#        submitStats: submitStatsGlobal {
+#          acSubmissionNum {
+#            difficulty
+#            count
+#            submissions
+#          }
+#        }
+#      }
+#    }
+#    """ % username
+# response = requests.post(url, json={'query': query})
+#
+# if response.status_code == 200:
+#     data = response.json()
+#     print(data)
+# else:
+#     print(f"Error: {response.status_code}")
+
+
 url = "https://leetcode.com/graphql"
 query = """
-   {
-     matchedUser(username: "%s") {
-       submitStats: submitStatsGlobal {
-         acSubmissionNum {
-           difficulty
-           count
-           submissions
-         }
-       }
-     }
-   }
-   """ % username
-response = requests.post(url, json={'query': query})
+           {
+               userContestRanking(username:  "%s")  {
+                    rating
+                }
+           }
+           """ % username
 
-if response.status_code == 200:
-    data = response.json()
-    print(data)
-else:
-    print(f"Error: {response.status_code}")
+response = requests.post(url, json={'query': query})
+print(response.json())
